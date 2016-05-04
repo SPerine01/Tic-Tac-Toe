2db4ba73b7757f8fc01569148fc5bb735d3fed01
Sumayyah Perine <sperine01@gmail.com>
2016-05-03 23:59:15 -0400

    adding js for functionality, modify html and css

---
+++ assets/tic_tac_toe.js
@@ -0,0 +1,17 @@
+var images = document.getElementByTagName("img");
+var startButty = document.getElementById("butty");
+
+var myImages = {
+	empty: "empty.jpg",
+	x: "x.jpg",
+	o: "o.jpg",
+	x_win: "xwin.jpg",
+	o_win: "owin.jpg"
+};
+
+// to clear the button action
+startButty.onclick = () => {
+	for (var i = Things.length - 1; i >= 0; i--) {
+		Things[i]
+	};
+}
