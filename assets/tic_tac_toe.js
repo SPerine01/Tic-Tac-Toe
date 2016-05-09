f9be9bf152af54a50fdca968e883f42718e4598b
Sumayyah Perine <sperine01@gmail.com>
2016-05-09 13:45:40 -0400

    remove old js

--- assets/tic_tac_toe.js
+++ assets/tic_tac_toe.js
@@ -1,17 +1,7 @@
-var images = document.getElementByTagName("img");
-var startButty = document.getElementById("butty");
+function startGame() {
+	document.turn = "X";
+}
 
-var myImages = {
-	empty: "empty.jpg",
-	x: "x.jpg",
-	o: "o.jpg",
-	x_win: "xwin.jpg",
-	o_win: "owin.jpg"
-};
-
-// to clear the button action
-startButty.onclick = () => {
-	for (var i = Things.length - 1; i >= 0; i--) {
-		Things[i]
-	};
+function theMessages(msg) {
+	
 }
