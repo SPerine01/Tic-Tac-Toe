404a9be968bade050fd7a6f45b1040157ae75918
Sumayyah Perine <sperine01@gmail.com>
2016-05-10 19:37:55 -0400

    add game functionality with js

--- assets/tic_tac_toe.js
+++ assets/tic_tac_toe.js
@@ -5,3 +5,18 @@ function startGame() {
 function theMessages(msg) {
 	document.getElementById("message").innerText = msg;
 }
+
+function nextMove(box) {
+	box.innerText = document.turn;
+	nextTurn();
+}
+
+function nextTurn() {
+	if (document.turn == "X") {
+		document.turn = "O";
+	} else {
+		document.turn = "X";
+	}
+
+	theMessages("It's " + document.turn + "'s turn now!");
+}
