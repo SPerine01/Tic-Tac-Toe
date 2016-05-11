07f96fd0571101cb8788e767322f89bcd18a9944
Sumayyah Perine <sperine01@gmail.com>
2016-05-11 01:13:58 -0400

    edit stylesheets, add more js functionality to game

--- assets/tic_tac_toe.js
+++ assets/tic_tac_toe.js
@@ -2,15 +2,22 @@ function startGame() {
 	document.turn = "X";
 }
 
+// display message
 function theMessages(msg) {
 	document.getElementById("message").innerText = msg;
 }
 
+// checks for the player's turn
 function nextMove(box) {
-	box.innerText = document.turn;
-	nextTurn();
+	if (box.innerText == "") {
+		box.innerText = document.turn;
+		nextTurn();
+	} else {
+		theMessages("Hey, you can't move there, silly goose!");
+	}
 }
 
+// checks for the next player's turn after the initial move
 function nextTurn() {
 	if (document.turn == "X") {
 		document.turn = "O";
@@ -19,4 +26,15 @@ function nextTurn() {
 	}
 
 	theMessages("It's " + document.turn + "'s turn now!");
+}
+
+// to check each row for x/o option
+function checkRow(x, y, z, move) {
+	var result = false;
+if () {
+
+} else {
+
+}
+
 }
